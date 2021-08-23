1ecbc1972ea03ddfcfaa6f4da67da3adf7de0d13
viktoriakonya <viktoria.konya@gmail.com>
2021-08-23 22:31:23 +0200

    Hello file was created

---
+++ exam/viktoria_konya/IT/my_script.sh
@@ -0,0 +1,8 @@
+# Change directory 
+cd ~/ba-pre-session-2020-archive/exam/viktoria_konya/IT
+# Create folder
+mkdir made_by_my_script
+cd  made_by_my_script
+nano hello.txt
+echo "Hello World" > hello.txt
+exit
